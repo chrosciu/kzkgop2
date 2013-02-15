051679baf4c76361b1706381d4f9be26a26cd7df
Marcin Chrost <mchrost@neubloc.net>
2013-02-15 09:52:30 +0100

    new scopes added

--- app/models/scope.rb
+++ app/models/scope.rb
@@ -8,9 +8,17 @@ class Scope
     new(id: 12, name: 'Robocze'),
     new(id: 13, name: 'Soboty'),
     new(id: 14, name: 'Niedziele i swieta'),
+    new(id: 15, name: 'Wigilia'),
+    new(id: 16, name: 'Sylwester'),
     new(id: 17, name: 'Robocze szkolne'),
     new(id: 18, name: 'Wolne'),
-    new(id: 19, name: 'Robocze w ferie i wakacje')
+    new(id: 19, name: 'Robocze w ferie i wakacje'),
+    new(id: 20, name: 'Dni wolne od pracy w CH'),
+    new(id: 21, name: 'Robocze szkolne i w ferie'),
+    new(id: 22, name: 'Robocze w wakacje'),
+    new(id: 23, name: 'Wszystkich Swietych'),
+    new(id: 38, name: 'Codziennie'),
+    new(id: 41, name: 'Dni wolne poza wolnymi od pracy w CH')
   ]
 
   class << self
